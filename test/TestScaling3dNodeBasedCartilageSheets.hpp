a50b6e6b161ec9f51ede6cd9366d795d0427373d
Sonja Mathias <sonja.mathias@it.uu.se>
2017-01-26 15:01:19 +0100

    Added a helper class for generating the coordinates for the nodes of the cell sheet. Currently there's a bug with generating cells manually.

--- test/TestScaling3dNodeBasedCartilageSheets.hpp
+++ test/TestScaling3dNodeBasedCartilageSheets.hpp
@@ -33,49 +33,92 @@ class TestScaling3dNodeBasedCartilageSheets : public AbstractCellBasedTestSuite
 {
 public:
   
+  /**
+   * Tests the 3D node-based cartilage sheet simulation
+   */
   void Test3dNodeBasedCartilageSheet() throw(Exception)
   {
 
     bool random_seed = true;
-    unsigned n_layers = 1;
-    unsigned n_cells_per_layer = 5;
-    unsigned n_differentiated_cells_on_side = 0;
+    unsigned n_cells_wide = 3;
+    unsigned n_cells_deep = 2;
+    unsigned n_cells_high = 1;
+    unsigned n_differentiated_cells_width = 0;
+    unsigned n_differentiated_cells_depth = 0;
     bool random_birth_times = true;
     
     double spring_stiffness = 1.0;
     double upper_boundary_plane = 4.0;
     double simulation_endtime = 40.0;
     
-    std::string output_directory = "3dNodeBasedCartilageSheet/TestTutorialSetup/";
+    std::string output_directory = "3dNodeBasedCartilageSheet/TestGenerateNodesWithAncestors/";
     
     Setup3dNodeBasedCartilageSheet(random_seed, 
-				  n_cells_per_layer,
-				  n_layers,
-				  n_differentiated_cells_on_side,
-				  random_birth_times,
-				  spring_stiffness,
-				  upper_boundary_plane,
-				  output_directory,
-				  simulation_endtime );
+				   n_cells_wide,
+				   n_cells_deep,
+				   n_cells_high,
+				   n_differentiated_cells_width,
+				   n_differentiated_cells_depth,
+				   random_birth_times,
+				   spring_stiffness,
+				   upper_boundary_plane,
+				   output_directory,
+				   simulation_endtime );
     
   }
 
+  /**
+   * Minimal testing for the generation of the node coordinates
+   */
+  void Test3dNodeGeneration() throw(Exception)
+  {
+    unsigned n_nodes_width = 3;
+    unsigned n_nodes_depth = 2;
+    unsigned n_nodes_height = 1;
+    
+    std::vector<Node<3>*> nodes;
+    GenerateNodes(nodes, n_nodes_width,n_nodes_depth,n_nodes_height);
+     
+    TS_ASSERT_EQUALS(nodes.size(),n_nodes_width*n_nodes_depth*n_nodes_height );    
+    
+    for(unsigned i = 0; i< nodes.size(); i++)
+    {
+      c_vector<double, 3> coordinates = nodes[i]->rGetLocation();
+      
+      TS_ASSERT_LESS_THAN_EQUALS(coordinates[0], 2.0);
+      TS_ASSERT_LESS_THAN_EQUALS(coordinates[1], 1.0);
+      TS_ASSERT_EQUALS(coordinates[2], 0.0);
+    }
+  }
+  
 private:
+  
+  /**
+   * Sets up the simulation for the specified input parameters
+   */
   void Setup3dNodeBasedCartilageSheet(bool random_seed, 
-				    unsigned n_cells_per_layer,
-				    unsigned n_layers,
-				    unsigned n_differentiated_cells_on_side,
+				    unsigned n_cells_wide,
+				    unsigned n_cells_deep,
+				    unsigned n_cells_high,
+				    unsigned n_differentiated_cells_width,
+				    unsigned n_differentiated_cells_depth,
 				    bool random_birth_times,
 				    double spring_stiffness,
 				    double upper_boundary_plane,
 				    std::string output_directory,
 				    double simulation_endtime ) throw(Exception)
   {
+    /** The next line is needed because this not designed to be run in parallel */
+    EXIT_IF_PARALLEL;
+    
     CellBasedEventHandler::Enable();
     std::stringstream ss;
-    ss << n_cells_per_layer << "/";
-    ss << n_layers << "/";
+    ss << n_cells_wide << "/";
+    ss << n_cells_deep << "/";
+    ss << n_cells_high << "/";
     ss << upper_boundary_plane << "/";
+    
+    unsigned n_cells_per_layer = n_cells_wide*n_cells_deep;
 
     
     // Reseed the number generator so that different runs will actually produce different results
@@ -87,11 +130,8 @@ private:
     }
     std::string filenameaddon_str = ss.str();
     
-    std::vector<Node<3>*> nodes;
-    nodes.push_back(new Node<3>(0u,  false,  0.5, 0.0, 0.0));
-    nodes.push_back(new Node<3>(1u,  false,  -0.5, 0.0, 0.0));
-    nodes.push_back(new Node<3>(2u,  false,  0.0, 0.5, 0.0));
-    nodes.push_back(new Node<3>(3u,  false,  0.0, -0.5, 0.0));
+    std::vector<Node<3>*> nodes;   
+    GenerateNodes(nodes, n_cells_wide,n_cells_deep,n_cells_high);
     
     NodesOnlyMesh<3> mesh;
     mesh.ConstructNodesWithoutMesh(nodes, 1.5);
@@ -100,53 +140,64 @@ private:
     MAKE_PTR(StemCellProliferativeType, p_stem_type); 
     MAKE_PTR(DifferentiatedCellProliferativeType, p_diff_type); 
     MAKE_PTR(WildTypeCellMutationState, p_state); 
-    CellsGenerator<StochasticDurationGenerationBasedCellCycleModel, 3> cells_generator;
-    cells_generator.GenerateBasicRandom(cells, mesh.GetNumNodes(), p_stem_type);
+    //CellsGenerator<StochasticDurationGenerationBasedCellCycleModel, 3> cells_generator;
+    //cells_generator.GenerateBasicRandom(cells, mesh.GetNumNodes(), p_stem_type);
+     
+
+    StochasticDurationGenerationBasedCellCycleModel* p_cell_cycle_model = new StochasticDurationGenerationBasedCellCycleModel;
+    // we could set maxTransitGenerations here.
+    //p_cell_cycle_model->SetMaxTransitGenerations(4);
     
-//     unsigned n_cells = location_indices.size();
-//     std::vector<CellPtr> cells_current_layer;
-// 
-//     //layer of differentiated and stem cells
-//     for (unsigned i=0; i<n_cells_per_layer; i++)
-//     {
-//       StochasticDurationGenerationBasedCellCycleModel* p_cell_cycle_model = new StochasticDurationGenerationBasedCellCycleModel;
-//       // we could set maxTransitGenerations here.
-//       //p_cell_cycle_model->SetMaxTransitGenerations(4);
-//       CellPtr p_cell(new Cell(p_state, p_cell_cycle_model));
-//       if(i<n_differentiated_cells_on_side || i>=n_cells_per_layer-n_differentiated_cells_on_side)
-//       {
-// 	p_cell->SetCellProliferativeType(p_diff_type);
-//       }
-//       else
-//       {
-// 	p_cell->SetCellProliferativeType(p_stem_type);
-// 	MAKE_PTR_ARGS(CellAncestor, p_cell_ancestor, (i));
-//         p_cell->SetAncestor(p_cell_ancestor);
-// 	if(random_birth_times)
-// 	{
-// 	  double birth_time = -p_cell_cycle_model->GetAverageStemCellCycleTime()*RandomNumberGenerator::Instance()->ranf();
-// 	  p_cell->SetBirthTime(birth_time);
-// 	}
-// 	else
-//         {
-//           p_cell->SetBirthTime(-20.0); //Average stem cell cycle time is 24.0 with default values 
-//                                       //Now we don't have to wait forever for cell divisions to start
-//         }
-// 
-// 
-//       }
-//       //p_cell->InitialiseCellCycleModel();
-//       cells.push_back(p_cell);
-//     }
+    for(unsigned k = 0; k<n_cells_per_layer; k++)
+    {
+      CellPtr p_cell(new Cell(p_state, p_cell_cycle_model));
+      p_cell->SetCellProliferativeType(p_diff_type);
+      cells.push_back(p_cell);
+    }
 //     
-//     // five more layers of differentiated cells
-//     cells_generator.GenerateBasicRandom(cells_current_layer, (n_layers-1)*n_cells_per_layer, p_diff_type); 
-//     cells.insert(cells.end(),cells_current_layer.begin(),cells_current_layer.end());
+//     //layer of differentiated and stem cells
+//     for (unsigned j=0; j<n_cells_deep; j++)
+//     {
+//       for (unsigned i=0; i<n_cells_wide; i++)
+//       {
+// 	CellPtr p_cell(new Cell(p_state, p_cell_cycle_model));
+// 	
+// // 	// padding of differentiated cells
+// // 	if(i<n_differentiated_cells_width || i>=n_cells_wide-n_differentiated_cells_width || j<n_differentiated_cells_depth || i>=n_cells_deep-n_differentiated_cells_depth)
+// // 	{
+// // 	  p_cell->SetCellProliferativeType(p_diff_type);
+// // 	}
+// // 	else
+// // 	{
+// 	  p_cell->SetCellProliferativeType(p_stem_type);
+// 	  MAKE_PTR_ARGS(CellAncestor, p_cell_ancestor, (i+j*n_cells_wide));
+// 	  p_cell->SetAncestor(p_cell_ancestor);
+// 	  if(random_birth_times)
+// 	  {
+// 	    double birth_time = -p_cell_cycle_model->GetAverageStemCellCycleTime()*RandomNumberGenerator::Instance()->ranf();
+// 	    p_cell->SetBirthTime(birth_time);
+// 	  }
+// 	  else
+// 	  {
+// 	    p_cell->SetBirthTime(-20.0); //Average stem cell cycle time is 24.0 with default values 
+//                                       //Now we don't have to wait forever for cell divisions to start
+// 	  }
+// 	  
+// // 	}
+// 	cells.push_back(p_cell);
+//       }
+//     }
+    
+    // n_cells_high-1 more layers of differentiated cells
+//     std::vector<CellPtr> cells_extra_layers;
+//     cells_generator.GenerateBasicRandom(cells_extra_layers, (n_cells_high-1)*n_cells_per_layer, p_diff_type); 
+//     cells.insert(cells.end(),cells_extra_layers.begin(),cells_extra_layers.end());
 
     NodeBasedCellPopulation<3> cell_population(mesh, cells); 
     cell_population.AddCellWriter<CellAncestorWriter>();
 
     OffLatticeSimulation3dDirectedDivision simulator(cell_population);
+    //OffLatticeSimulation<3> simulator(cell_population);
     simulator.SetOutputDirectory(output_directory+filenameaddon_str);
     simulator.SetEndTime(simulation_endtime); //hours
     simulator.SetSamplingTimestepMultiple(12);
@@ -182,6 +233,34 @@ private:
         {
             delete nodes[i];
         }
+  }
+  
+  /**
+   * Generates nodes for a 3D cell sheet a given number of cells wide, deep and high.
+   */
+  void GenerateNodes(std::vector<Node<3>*> & rNodes, 
+		     unsigned n_nodes_width, 
+		     unsigned n_nodes_depth,
+		     unsigned n_nodes_height) throw(Exception)
+  {
+    
+    rNodes.clear();
+    unsigned n_nodes = n_nodes_width*n_nodes_depth*n_nodes_height;
+    rNodes.reserve(n_nodes);
+    
+    unsigned id = 0;
+    
+    for(unsigned k=0; k<n_nodes_height; k++)
+    {
+      for(unsigned j=0; j<n_nodes_depth; j++)
+      {
+	for(unsigned i=0; i<n_nodes_width; i++)
+	{
+	  rNodes.push_back(new Node<3>(id,  false,  (double) i, (double) j, (double) k));
+	  id++;
+	}
+      }
+    }
   }
   
 };
