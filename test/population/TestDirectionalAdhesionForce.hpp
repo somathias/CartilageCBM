bac8b1586031af977171c7401069dbaf47837873
Sonja Mathias <sonja.mathias@it.uu.se>
2018-03-01 17:18:30 +0100

    Improved the test to include the simple directional case for perichondrial cells.

--- test/population/TestDirectionalAdhesionForce.hpp
+++ test/population/TestDirectionalAdhesionForce.hpp
@@ -66,15 +66,12 @@ public:
 		DirectionalAdhesionForce force;
 
 		// Test default values
-		TS_ASSERT_DELTA(force.GetBaselineAdhesionMultiplier(), 0.1,
-				1e-6);
+		TS_ASSERT_DELTA(force.GetBaselineAdhesionMultiplier(), 0.1, 1e-6);
 
 		// Test set/get method
 		force.SetBaselineAdhesionMultiplier(0.0);
 
-		TS_ASSERT_DELTA(
-				force.GetBaselineAdhesionMultiplier(), 0.0,
-				1e-6);
+		TS_ASSERT_DELTA(force.GetBaselineAdhesionMultiplier(), 0.0, 1e-6);
 
 		force.SetBaselineAdhesionMultiplier(0.1);
 		double heterotypic_multiplier = 4.0;
@@ -177,8 +174,160 @@ public:
 
 	}
 
-	void TestDirectionalAdhesionForceOutputParameters()
-	{
+	void TestDirectionalAdhesionForcePerichondrialCase() {
+
+		std::vector<Node<3>*> nodes;
+		nodes.push_back(new Node<3>(0u, false, 0.0, 0.0, 0.0));
+		nodes.push_back(new Node<3>(1u, false, -1.0, 0.0, 0.0));
+		nodes.push_back(new Node<3>(2u, false, 1.0, 0.0, 0.0));
+		nodes.push_back(new Node<3>(3u, false, 0.25, 0.0, 1.25));
+		//     nodes.push_back(new Node<3>(3u,  false,  0.0, -0.5, 0.0));
+
+		NodesOnlyMesh<3> mesh;
+		mesh.ConstructNodesWithoutMesh(nodes, 1.5);
+
+		std::vector<CellPtr> cells;
+		MAKE_PTR(TransitCellProliferativeType, p_transit_type);
+		CellsGenerator<FixedDurationGenerationBasedCellCycleModel, 3> cells_generator;
+		cells_generator.GenerateBasicRandom(cells, mesh.GetNumNodes(),
+				p_transit_type);
+
+		NodeBasedCellPopulation<3> cell_population(mesh, cells);
+
+		boost::shared_ptr<AbstractCellProperty> p_perichondrial(
+				cell_population.GetCellPropertyRegistry()->Get<
+						PerichondrialCellTissueType>());
+
+		// all cells are perichondrial cells
+		cell_population.GetCellUsingLocationIndex(0)->AddCellProperty(
+				p_perichondrial);
+		cell_population.GetCellUsingLocationIndex(1)->AddCellProperty(
+				p_perichondrial);
+		cell_population.GetCellUsingLocationIndex(2)->AddCellProperty(
+				p_perichondrial);
+		cell_population.GetCellUsingLocationIndex(3)->AddCellProperty(
+				p_perichondrial);
+
+		// Create force
+		DirectionalAdhesionForce force;
+
+		// Test default values
+		TS_ASSERT_DELTA(force.GetBaselineAdhesionMultiplier(), 0.1, 1e-6);
+
+		double homotypic_multiplier = 4.0;
+		force.SetHomotypicPerichondrialSpringConstantMultiplier(homotypic_multiplier);
+
+		double alpha = 12.5;
+		force.SetAlpha(alpha);
+		TS_ASSERT_DELTA(force.GetAlpha(), alpha, 1e-6);
+
+		double repulsion_spring_stiffness = 1.0;
+		force.SetRepulsionSpringStiffness(repulsion_spring_stiffness);
+		TS_ASSERT_DELTA(force.GetRepulsionSpringStiffness(),
+				repulsion_spring_stiffness, 1e-6);
+
+		// Initialise a vector of node forces
+		for (unsigned i = 0; i < cell_population.GetNumNodes(); i++) {
+			cell_population.GetNode(i)->ClearAppliedForce();
+		}
+
+		// Move a node along the x-axis and calculate the force exerted on a neighbour
+		CellPtr my_cell = cell_population.rGetCells().front();
+		c_vector<double, 3> my_old_coords =
+				cell_population.GetLocationOfCellCentre(my_cell);
+		ChastePoint<3> new_coords;
+		new_coords.rGetLocation()[0] = my_old_coords[0] + 0.25;
+		new_coords.rGetLocation()[1] = my_old_coords[1];
+		new_coords.rGetLocation()[2] = my_old_coords[2];
+		unsigned node_index = cell_population.GetLocationIndexUsingCell(
+				my_cell);
+		cell_population.SetNode(node_index, new_coords);
+
+		TS_ASSERT_DELTA(cell_population.GetNode(0)->rGetLocation()[0], 0.25,
+				1e-4);
+		TS_ASSERT_DELTA(cell_population.GetNode(0)->rGetLocation()[1], 0.0,
+				1e-4);
+		TS_ASSERT_DELTA(cell_population.GetNode(0)->rGetLocation()[2], 0.0,
+				1e-4);
+
+		double spring_stiffness_adhesion = force.GetMeinekeSpringStiffness();
+		double spring_stiffness_repulsion = force.GetRepulsionSpringStiffness();
+
+		// Calculate the force between nodes 0 and 2
+		c_vector<double, 3> force_contribution =
+				force.CalculateForceBetweenNodes(0, 2, cell_population);
+		for (unsigned j = 0; j < 3; j++) {
+			assert(!std::isnan(force_contribution[j]));
+		}
+
+		TS_ASSERT_DELTA(force_contribution[0],
+				spring_stiffness_repulsion * log(0.75), 1e-4); //repulsion force, hence no multiplier
+		TS_ASSERT_DELTA(force_contribution[1], 0.0, 1e-4);
+		TS_ASSERT_DELTA(force_contribution[2], 0.0, 1e-4);
+
+		// Calculate the force between nodes 0 and 1
+		force_contribution = force.CalculateForceBetweenNodes(0, 1,
+				cell_population);
+		for (unsigned j = 0; j < 3; j++) {
+			assert(!std::isnan(force_contribution[j]));
+		}
+
+		TS_ASSERT_DELTA(force_contribution[0],
+				-0.25 * homotypic_multiplier * spring_stiffness_adhesion
+						* exp(-alpha * 0.25), 1e-4); //adhesion force, hence homotypic multiplier and directional_multiplier, the latter should be one
+		TS_ASSERT_DELTA(force_contribution[1], 0.0, 1e-4);
+		TS_ASSERT_DELTA(force_contribution[2], 0.0, 1e-4);
+
+		// Calculate the force between nodes 0 and 3
+		force_contribution = force.CalculateForceBetweenNodes(0, 3,
+				cell_population);
+		for (unsigned j = 0; j < 3; j++) {
+			assert(!std::isnan(force_contribution[j]));
+		}
+
+		TS_ASSERT_DELTA(force_contribution[0], 0.0, 1e-4);
+		TS_ASSERT_DELTA(force_contribution[1], 0.0, 1e-4);
+		TS_ASSERT_DELTA(force_contribution[2],
+						0.25 * homotypic_multiplier * 0.1 * spring_stiffness_adhesion
+								* exp(-alpha * 0.25), 1e-4); //adhesion force, hence homotypic multiplier and directional_multiplier, the latter should be the baseline_adhesion_multiplier
+
+		cell_population.Update(false);
+		std::vector<std::pair<Node<3>*, Node<3>*> >& r_node_pairs =
+				cell_population.rGetNodePairs();
+		TS_ASSERT_EQUALS(r_node_pairs.size(), 6);
+//
+//		// Initialise a vector of node forces
+//		for (unsigned i = 0; i < cell_population.GetNumNodes(); i++) {
+//			cell_population.GetNode(i)->ClearAppliedForce();
+//		}
+//		force.AddForceContribution(cell_population);
+//
+//		TS_ASSERT_DELTA(cell_population.GetNode(0)->rGetAppliedForce()[0],
+//				spring_stiffness_repulsion * log(0.75)
+//						- 0.25 * homotypic_multiplier
+//								* spring_stiffness_adhesion
+//								* exp(-alpha * 0.25), 1e-4);
+//		TS_ASSERT_DELTA(cell_population.GetNode(0)->rGetAppliedForce()[1], 0.0,
+//				1e-4);
+//		TS_ASSERT_DELTA(cell_population.GetNode(0)->rGetAppliedForce()[2], 0.0,
+//				1e-4);
+//		TS_ASSERT_DELTA(cell_population.GetNode(1)->rGetAppliedForce()[0],
+//				0.25 * homotypic_multiplier * spring_stiffness_adhesion
+//						* exp(-alpha * 0.25), 1e-4);
+//		TS_ASSERT_DELTA(cell_population.GetNode(1)->rGetAppliedForce()[1], 0.0,
+//				1e-4);
+//		TS_ASSERT_DELTA(cell_population.GetNode(1)->rGetAppliedForce()[2], 0.0,
+//				1e-4);
+//		TS_ASSERT_DELTA(cell_population.GetNode(2)->rGetAppliedForce()[0],
+//				-spring_stiffness_repulsion * log(0.75), 1e-4);
+//		TS_ASSERT_DELTA(cell_population.GetNode(2)->rGetAppliedForce()[1], 0.0,
+//				1e-4);
+//		TS_ASSERT_DELTA(cell_population.GetNode(2)->rGetAppliedForce()[2], 0.0,
+//				1e-4);
+
+	}
+
+	void TestDirectionalAdhesionForceOutputParameters() {
 		EXIT_IF_PARALLEL;
 		std::string output_directory = "TestForcesOutputParameters";
 		OutputFileHandler output_file_handler(output_directory, false);
